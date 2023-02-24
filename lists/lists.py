75b95b3497aa42947a14c7c5fc723a70a9635a04
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-02-24 12:37:12 +0300

    Add function "replace" for lists. Add diff for black results

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,20 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        output_list = []
+        max_elem = 0
+
+        for elem in input_list:
+            if elem > max_elem:
+                max_elem = elem
+
+        for elem in input_list:
+            if elem > 0 and elem != max_elem:
+                output_list.append(max_elem)
+            else:
+                output_list.append(elem)
+
+        return output_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
