03f6bd66abb2505b41018c93cd24c52a810c0ef5
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-02-24 16:05:32 +0300

    Add "rating" and "chars_count" functions

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,17 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        def get_rating(movie: dict) -> Union[float, None]:
+            if len(movie["country"].split(",")) > 1:
+                if movie["rating_kinopoisk"]:
+                    return float(movie["rating_kinopoisk"])
+            return None
+
+        list_of_ratings = list(
+            filter(lambda rate: True if rate else False, map(get_rating, list_of_movies))
+        )
+        return sum(list_of_ratings) / len(list_of_ratings)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +38,18 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        def get_letters_number(movie: dict) -> int:
+            count = 0
+            for letter in movie["name"]:
+                if letter == "и":
+                    count += 1
+            return count
+
+        movies_rated = filter(
+            lambda movie: float(movie["rating_kinopoisk"]) >= rating
+            if movie["rating_kinopoisk"]
+            else False,
+            list_of_movies,
+        )
+        return sum(list(map(get_letters_number, movies_rated)))
